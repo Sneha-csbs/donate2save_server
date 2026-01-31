db23d2b39cd4bfd89db703949d65eaba1d41e6cc
kowsikGkk <kowsikgunasekaran@gmail.com>
2026-01-31 11:33:19 +0530

    final commit

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ import notificationRoute from "./Routes/NotificationRoute.js";
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://donate2save.netlify.app', 'http://localhost:3000', 'http://localhost:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // DB Connection
